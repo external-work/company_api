89082963dd355ac698a46e9b96ab0f3c58c7e5cc
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-04-30 01:12:02 +0530

    first push

--- Getdata/urls.py
+++ Getdata/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/webhook/<companyname>/', views.send_data, name='company'),
+    path('webhook/<companyname>/', views.send_data, name='company'),
 ]
