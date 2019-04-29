79f865e5774ecbbe34f1af7ba0f45319174b3443
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-04-30 01:34:54 +0530

    first push

--- Getdata/views.py
+++ Getdata/views.py
@@ -5,7 +5,7 @@ def send_data(request, companyname):
 	data = generate_data(companyname)
 	# return JsonResponse({'data':data})
 	data = "The company " + data + " contains these information."
-	with open("logging.txt","wa") as file:
+	with open("logging.txt","a") as file:
 		temp = "Sending data about " + data
 		file.write(temp)
 	return HttpResponse(data)
