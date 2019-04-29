1c302f10ca499b4c72dc740dba5548bff92bf77c
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-04-30 00:35:59 +0530

    first push

--- Getdata/views.py
+++ Getdata/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-import json
 # Create your views here.
 def send_data(request, companyname):
-	main_data = json.loads(request.body)
-	custom_data = main_data['parameters']['Company']
 	data = generate_data(companyname)
 	# return JsonResponse({'data':data})
-	data = "The company " + custom_data + " contains these information."
+	data = "The company " + data + " contains these information."
 	return HttpResponse(data)
 
 def generate_data(company):
