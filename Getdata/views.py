be95e52ead9a5475ce839169fd092814e62e6409
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-04-30 01:22:45 +0530

    first push

--- Getdata/views.py
+++ Getdata/views.py
@@ -5,6 +5,7 @@ def send_data(request, companyname):
 	data = generate_data(companyname)
 	# return JsonResponse({'data':data})
 	data = "The company " + data + " contains these information."
+	print("Sent data about" + data)
 	return HttpResponse(data)
 
 def generate_data(company):
